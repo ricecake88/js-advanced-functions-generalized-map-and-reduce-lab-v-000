4ba83eddc9a8f254b75ff5c56d77aa77d4f5287a
Grace Shih <vocalise@gmail.com>
2020-02-04 06:53:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(sourceArray, mapFunction) {
+  let newArray = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    newArray.push(mapFunction(sourceArray[i]));
+  }
+  return newArray;
+}
+
+function reduce(sourceArray, reduceFunction, startValue=0) {
+  let result = startValue;
+  for (let i = 0; i < sourceArray.length; i++) {
+      result = reduceFunction(sourceArray[i], startValue+3);
+  }
+  return result;
+}
